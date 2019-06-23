3d69fa2c6b05332f57a9751b947948d98f833d88
Harshit Kakkar <kharshit0@gmail.com>
2019-06-24 02:45:31 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 
 const PORT = process.env.PORT || 8765
 
-app.use('/app',express.static(__dirname + '/public'))
+app.use('/',express.static(__dirname + '/public'))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
