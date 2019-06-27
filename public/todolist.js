916dcfb39d6240e8cbb39ff294f3d5763df28078
Harshit Kakkar <kharshit0@gmail.com>
2019-06-27 06:32:30 +0530

    Minor changes

--- public/todolist.js
+++ public/todolist.js
@@ -13,51 +13,19 @@ $(() => {
                         $('<li>').text(task.name )
 
                     )
-                    // .append(
-                    //     $('<br>')
-                    // )
                     .append(
                         $('<p>').text('Description: ' + task.description)
                     )
                     .append(
                         $('<button>').attr('class','del').text('DEL').click(()=> {
                             
-                        //     $(this).parent().remove();
-                        //     console.log($(this).parent())
-                        //     // refreshlist()
-                            
-                        // deleteitem();
-                        
-                        removetask(task.id);
-                        
-                        
-                            
-                         })
-                        
-                        
+                        removetask(task.id);    
+                         }) 
                     )
                 }
             }
         )
     }
-
-    // $('.del').on('click',function(){
-    //     $(this).parent().remove();
-    //     console.log($(this).parent())
-    // })
-
-//     function deleteitem(){
-//         // adda common class to all the buttons
-//     let deleteBtn = document.getElementsByClassName("del");
-//     // converting html collection to array, to use array methods
-//     Array.prototype.slice.call(deleteBtn).forEach(function(item) {
-//     // iterate and add the event handler to it
-//     item.addEventListener("click", function(e) {
-//         e.target.parentNode.remove()
-//     });
-
-// })
-//     }
 
 
     async function removetask(elem){
@@ -67,7 +35,7 @@ $(() => {
             // url:`/todos/${elem.data('task.id')}`
             url:'/todos/'+elem
         })
-        // elem.remove();
+        
         refreshlist();
     }
     
